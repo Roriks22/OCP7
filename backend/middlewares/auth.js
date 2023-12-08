9e4c7ad3562d4357294807820c317c4866198965
romain jacquesson <romain.jacquesson@gmail.com>
2023-12-08 19:25:14 +0100

    ajout_webp

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,9 +2,12 @@ const jwt = require('jsonwebtoken');
  
 module.exports = (req, res, next) => {
    try {
+    // On récupére le token.
        const token = req.headers.authorization.split(' ')[1];
+    // Utilisation du jwt.verify qui permet l'échange sécurité de tokens entre plusieurs parties en faisant une vérification de l'intégralité et de l'authenticité des données. 
        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
        const userId = decodedToken.userId;
+    // On renvoie l'userId pour pouvoir l'utiliser dans les routes.
        req.auth = {
            userId: userId
        };
--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,18 +1,23 @@
 const multer = require('multer');
 
+// Création des Mime_Types pour gérer l'extension du fichier.
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
+  'image/webp': 'webp',
   'image/png': 'png'
 };
 
+// Utilisatio de diskStorage pour configurer le chemin et le nom de fichier pour les fichiers entrants.
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
+//   Création du nom du fichier.
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
+    // Création du nom du ficher avec la date de la mise en ligne.
     callback(null, name + Date.now() + '.' + extension);
   }
 });
